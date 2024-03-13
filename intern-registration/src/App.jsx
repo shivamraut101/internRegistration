c0a0c2637892cbd95cd9842063043afbe48cda99
shivamraut101 <shivamkumarraut12@gmail.com>
2024-03-13 23:55:46 +0530

    Layout and routes added

--- intern-registration/src/App.jsx
+++ intern-registration/src/App.jsx
@@ -1,15 +1,31 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ErrorPage } from './ErrorPage';
+import { GoogleAuth } from './GoogleAuth';
+import { useAuth0 } from '@auth0/auth0-react';
+import { MainDashboard } from './main dashboard/MainDashboard';
+import { Layout } from './Layout/Layout';
+
 
 function App() {
   const [count, setCount] = useState(0)
+ const {user} = useAuth0()
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+    <Layout>
+    <Routes>
+        <Route path='/' element={<GoogleAuth />} />
+        {
+          user && <Route path="main-dashboard" element={<MainDashboard/>} />
+        }
+        <Route path='*' element={<ErrorPage/>}/> 
+    </Routes>
+    </Layout>
+  </BrowserRouter>
   )
 }
 
