682145b251574a0259ce35a4ddc9e2a9bfe7872a
shivamraut101 <shivamkumarraut12@gmail.com>
2024-03-13 23:56:03 +0530

    Auth0 added

--- intern-registration/src/main.jsx
+++ intern-registration/src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Auth0Provider
+    domain="dev-x04tf7uf3dce71s4.us.auth0.com"
+    clientId="NulSlhId7FbB7Ldc6b8XktfLo3Ts2DHu"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
+  </Auth0Provider>,
   </React.StrictMode>,
 )
