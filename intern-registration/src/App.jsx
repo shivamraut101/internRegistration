17364b31632ed312e4b52aa0aefca62ac4a7269a
shivamraut101 <shivamkumarraut12@gmail.com>
2024-03-15 14:24:32 +0530

    PrivateRoute added

--- intern-registration/src/App.jsx
+++ intern-registration/src/App.jsx
@@ -1,31 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ErrorPage } from './ErrorPage';
 import { GoogleAuth } from './GoogleAuth';
 import { useAuth0 } from '@auth0/auth0-react';
 import { MainDashboard } from './main dashboard/MainDashboard';
 import { Layout } from './Layout/Layout';
+import { PersonalInfo } from './main dashboard/PersonalInfo';
 
 
 function App() {
   const [count, setCount] = useState(0)
  const {user} = useAuth0()
+ const { isAuthenticated } = useAuth0();
+ const PrivateRoute = ({ children }) => {
+   return isAuthenticated ? <>{children}</> : <Navigate to="/" />;
+ };
 
   return (
     <BrowserRouter>
     <Layout>
     <Routes>
-        <Route path='/' element={<GoogleAuth />} />
-        {
-          user && <Route path="main-dashboard" element={<MainDashboard/>} />
-        }
-        <Route path='*' element={<ErrorPage/>}/> 
-    </Routes>
+          <Route path="/" element={<GoogleAuth />} />
+          <Route
+            path="/main-dashboard"
+            element={
+              <PrivateRoute>
+                <MainDashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="personal-info"
+            element={
+              <PrivateRoute>
+                <PersonalInfo />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
     </Layout>
   </BrowserRouter>
+  
   )
 }
 
