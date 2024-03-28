9799fbac496146312f6e7422c49d6dd228077451
shivamraut101 <shivamkumarraut12@gmail.com>
2024-03-29 01:01:53 +0530

    Auth0 added

--- intern-registration/src/GoogleAuth.jsx
+++ intern-registration/src/GoogleAuth.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useNavigate } from 'react-router-dom';
 
 export const GoogleAuth = () => {
-    const { loginWithRedirect, user } = useAuth0();
-    const navigate  = useNavigate()
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-    <h1 className="text-3xl font-bold mb-8">Welcome to Intern Registration</h1>
-    {user && navigate('/main-dashboard')}
-    <button
-      onClick={() => loginWithRedirect()}
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-    >
-      Login with Auth0
-    </button>
-  </div>
-  )
-}
+    const { loginWithRedirect, isAuthenticated } = useAuth0();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            navigate('/main-dashboard');
+        }
+    }, [isAuthenticated, navigate]);
+
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-3xl font-bold mb-8">Welcome to Intern Registration</h1>
+            <button
+                onClick={() => loginWithRedirect()}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+                Login with Auth0
+            </button>
+        </div>
+    );
+};
