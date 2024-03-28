ecd7032ed3024ba46a9932f4be7fb43ddaf5d891
shivamraut101 <shivamkumarraut12@gmail.com>
2024-03-28 11:18:14 +0530

    display route

---
+++ backend/Routers Display/displayRoutes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const Intern = require("../internSchema");
+const { body, validationResult } = require("express-validator");
+
+
+// Route handler to fetch user information based on email
+router.get("/user-info/:email", async (req, res) => {
+    try {
+      const email = req.params.email;
+      const intern = await Intern.findOne({ 'personalInfo.email': email });
+  
+      // If user data exists, send it in the response
+      if (intern) {
+        res.json(intern.personalInfo);
+        console.log(intern.personalInfo)
+      } else {
+        res.status(404).json({ message: "User not found" });
+      }
+    } catch (error) {
+      console.error("Error fetching user information:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  });
+  
+
+  module.exports = router;
