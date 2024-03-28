62a61c38937f827073510e005a99f49519e84a03
shivamraut101 <shivamkumarraut12@gmail.com>
2024-03-29 01:01:12 +0530

    display passport image added

--- backend/Routers Display/displayRoutes.js
+++ backend/Routers Display/displayRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Intern = require("../internSchema");
+const {authorizeB2} = require('../b2Auth')
+const axios = require('axios');
 const { body, validationResult } = require("express-validator");
 
 
@@ -22,6 +24,37 @@ router.get("/user-info/:email", async (req, res) => {
       res.status(500).json({ message: "Internal server error" });
     }
   });
-  
+
+router.get("/get-image/:fileName", async (req, res) => {
+  try {
+      const b2 = await authorizeB2(); // Authorize B2 and get the B2 client instance
+      const fileName = req.params.fileName;
+
+      // Get temporary URL for the image
+      const response = await b2.downloadFileByName({
+          bucketName: 'basketHuntInternData',
+          fileName: fileName,
+          responseType: 'arraybuffer', // options are as in axios: 'arraybuffer', 'blob', 'document', 'json', 'text', 'stream'
+      });
+
+      // Extract headers from the response
+      const headers = response.config.headers;
+
+      // Make a GET request to the URL received
+      const imageDataResponse = await axios.get(response.config.url, {
+          responseType: 'arraybuffer', // Ensure the response type is correct
+          headers: headers // Pass the extracted headers
+      });
+
+      // Set the headers obtained from the response
+      res.set(imageDataResponse.headers);
+
+      // Send the image data in the response
+      res.send(imageDataResponse.data);
+  } catch (error) {
+      console.log("Error :: get-image ::", error);
+      res.status(500).json({ success: false, message: "Error fetching image" });
+  }
+});
 
   module.exports = router;
